898248abeac27d4fdb4be87a3f64769945788892
damonryon <damon.ryon@gmail.com>
2025-01-05 12:58:50 -0700

    dog cards displaying properly

--- src/Functional/FunctionalApp.tsx
+++ src/Functional/FunctionalApp.tsx
@@ -1,15 +1,31 @@
+import { useEffect, useState } from "react";
+import { Requests } from "../api";
 import { FunctionalCreateDogForm } from "./FunctionalCreateDogForm";
 import { FunctionalDogs } from "./FunctionalDogs";
 import { FunctionalSection } from "./FunctionalSection";
+import { Dog } from "../types";
 
 export function FunctionalApp() {
+  const [allDogs, setAllDogs] = useState<Dog[]>([]);
+
+  const fetchAndSetAllDogs = () => {
+    return Requests.getAllDogs().then(setAllDogs);
+  };
+
+  useEffect(() => {
+    fetchAndSetAllDogs();
+  }, []);
+
   return (
-    <div className="App" style={{ backgroundColor: "skyblue" }}>
+    <div
+      className="App"
+      style={{ backgroundColor: "skyblue" }}
+    >
       <header>
         <h1>pup-e-picker (Functional)</h1>
       </header>
       <FunctionalSection />
-      <FunctionalDogs />
+      <FunctionalDogs allDogs={allDogs} />
       <FunctionalCreateDogForm />
     </div>
   );
--- src/Functional/FunctionalDogs.tsx
+++ src/Functional/FunctionalDogs.tsx
@@ -1,13 +1,13 @@
 import { DogCard } from "../Shared/DogCard";
 import { dogPictures } from "../dog-pictures";
+import { Dog } from "../types";
 
-// Right now these dogs are constant, but in reality we should be getting these from our server
-export const FunctionalDogs = () => {
+export const FunctionalDogs = ({ allDogs }: { allDogs: Dog[] }) => {
   return (
     //  the "<> </>"" are called react fragments, it's like adding all the html inside
     // without adding an actual html element
     <>
-      <DogCard
+      {/* <DogCard
         dog={{
           id: 1,
           image: dogPictures.BlueHeeler,
@@ -26,67 +26,26 @@ export const FunctionalDogs = () => {
           alert("clicked empty heart");
         }}
         isLoading={false}
-      />
-      <DogCard
-        dog={{
-          id: 2,
-          image: dogPictures.Boxer,
-          description: "Example Description",
-          isFavorite: false,
-          name: "Cute Boxer",
-        }}
-        key={2}
-        onTrashIconClick={() => {
-          alert("clicked trash");
-        }}
-        onHeartClick={() => {
-          alert("clicked heart");
-        }}
-        onEmptyHeartClick={() => {
-          alert("clicked empty heart");
-        }}
-        isLoading={false}
-      />
-      <DogCard
-        dog={{
-          id: 3,
-          image: dogPictures.Chihuahua,
-          description: "Example Description",
-          isFavorite: false,
-          name: "Cute Chihuahua",
-        }}
-        key={3}
-        onTrashIconClick={() => {
-          alert("clicked trash");
-        }}
-        onHeartClick={() => {
-          alert("clicked heart");
-        }}
-        onEmptyHeartClick={() => {
-          alert("clicked empty heart");
-        }}
-        isLoading={false}
-      />
-      <DogCard
-        dog={{
-          id: 4,
-          image: dogPictures.Corgi,
-          description: "Example Description",
-          isFavorite: false,
-          name: "Cute Corgi",
-        }}
-        key={4}
-        onTrashIconClick={() => {
-          alert("clicked trash");
-        }}
-        onHeartClick={() => {
-          alert("clicked heart");
-        }}
-        onEmptyHeartClick={() => {
-          alert("clicked empty heart");
-        }}
-        isLoading={false}
-      />
+      /> */}
+      <section className="dog-section">
+        {allDogs.map((dog) => (
+          <div key={dog.id}>
+            <DogCard
+              dog={dog}
+              onTrashIconClick={() => {
+                alert("clicked trash");
+              }}
+              onHeartClick={() => {
+                alert("clicked heart");
+              }}
+              onEmptyHeartClick={() => {
+                alert("clicked empty heart");
+              }}
+              isLoading={false}
+            />
+          </div>
+        ))}
+      </section>
     </>
   );
 };
